8a6215d86d6082c6c98962d504bbcc2477358520
Viktoriia Fedotkina <victoria31f@gmail.com>
2021-02-18 00:50:22 +0300

    added typing training logic

--- src/components/Trainer/Trainer.js
+++ src/components/Trainer/Trainer.js
@@ -1,19 +1,73 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import './Trainer.css';
 
 function Trainer() {
-    const [text, setText] = React.useState('');
+    const [text, setText] = useState('');
+    const [countPressedCorrect, setCountPressedCorrect] = useState(0);
+    const [states, setStates] = useState([]);
+    const [shouldWriteState, setShouldWrite] = useState(true);
+    const [stop, setStop] = useState(false);
 
-    React.useEffect(() => {
-        fetch('https://baconipsum.com/api/?type=meat-and-filler&paras=2')
+    useEffect(() => {
+        fetch('https://baconipsum.com/api/?type=meat-and-filler&paras=1')
             .then((res) => res.json())
-            .then((text) => setText(text))
+            .then((text) => setText(text.join('').replace(/  /g, ' ').split('')))
             .catch((err) => console.log(err));
     }, [])
 
+    useEffect(() => {
+        window.addEventListener('keydown', handleKeyDown);
+
+        return () => {
+            window.removeEventListener('keydown', handleKeyDown);
+        };
+    }, [text, countPressedCorrect, stop, shouldWriteState])
+
+    const handleKeyDown = (e) => {
+        if(e.key !== 'Shift') {
+
+            if(e.key === text[countPressedCorrect]) {
+                setShouldWrite(true);
+                setStates((prevState) => {
+                    if(!shouldWriteState) {
+                        return [ ...prevState ];
+                    }
+                    return [
+                        ...prevState,
+                        true
+                    ]
+                });
+                setCountPressedCorrect((prevState) => (prevState + 1));
+                setStop(false);
+            } else {
+                if(stop) {
+                    return;
+                }
+                setStates((prevState) => ([
+                    ...prevState,
+                    false
+                ]));
+                setStop(true);
+                setShouldWrite(false);
+            }
+        }
+
+    }
+
     return (
         <div className="d-flex p-3 trainer bg-white border border-white rounded">
-            <div className="col">{text}</div>
+            <div className="col symbols">
+                {
+                    text && text.map((elem, idx) => {
+                        const state = states[idx];
+                        return (
+                            <span key={idx}
+                                  className={`${state === true ? 'symbols_passed' : state === false ? 'symbols_failed' : 'symbols_black'} ${countPressedCorrect === idx && 'symbols_current'}`}>
+                                {elem}</span>
+                        )
+                    })
+                }
+            </div>
             <div className="trainer__metrics col-3 d-flex flex-column align-content-center text-center">
                 <div className="trainer__metric metric">
                     <p className="metric__text"><i className="bi bi-stopwatch"/> скорость</p>

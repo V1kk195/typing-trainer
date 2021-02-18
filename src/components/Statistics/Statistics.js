3b12dcd7637e75dc5a69b8576463778123f05cc6
Viktoriia Fedotkina <victoria31f@gmail.com>
2021-02-18 21:04:17 +0300

    added accuracy stat

--- src/components/Statistics/Statistics.js
+++ src/components/Statistics/Statistics.js
@@ -1,8 +1,13 @@
 import React from "react";
+import { useSelector } from "react-redux";
 
 import './Statistics.css';
 
 function Statistics() {
+    const keys = useSelector(state => state.trainer);
+
+    const accuracy = keys.keysPressedCorrect ? Math.round(keys.keysPressedCorrect / keys.keysPressedAll * 100) : 0;
+
     return (
         <div className="statistics col-3 d-flex flex-column align-content-center text-center">
             <div className="statistics__metric metric">
@@ -11,7 +16,7 @@ function Statistics() {
             </div>
             <div className="statistics__metric metric">
                 <p className="metric__text"><i className="bi bi-record-circle"/> точность</p>
-                <span>100%</span>
+                <span>{accuracy}%</span>
             </div>
         </div>
     )
